a3fffed1dcbed10076b87c0d460559b1029f2249
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-06-14 20:12:47 +0200

    added initial application code

---
+++ G2/Class 1/ASPNETMVC-G2-L1/ASPNETMVC-G2-L1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNETMVC_G2_L1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
