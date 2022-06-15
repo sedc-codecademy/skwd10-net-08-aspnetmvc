35e0d5c9293ed9d7fd6a0e3afa30150fea7cd086
rpanchevski <risto.panchevski@gmail.com>
2022-06-15 20:51:13 +0200

    Class 02

---
+++ G1/Class02/DemoWebApp/DemoWebApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using DemoWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("[Controller]/prazna")]
+        public IActionResult Nothing()
+        {
+            return new EmptyResult();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
