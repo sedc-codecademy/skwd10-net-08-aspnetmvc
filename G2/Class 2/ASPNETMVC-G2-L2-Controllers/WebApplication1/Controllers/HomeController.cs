006f0d1e742af94e5a5ac926e9e1570855eee86a
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-06-16 19:13:15 +0200

    added pizza controller and endpoints

---
+++ G2/Class 2/ASPNETMVC-G2-L2-Controllers/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult HelloGTwo()
+        {
+            var item = new
+            {
+                Id = 1,
+                Text = "Hi Grupa 2. When are we going to go to Prilep to eat Skara?"
+            };
+
+            return Ok(item);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ G2/Class 2/ASPNETMVC-G2-L2-Controllers/WebApplication1/Controllers/PizzaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PizzApp.Controllers
+{
+    [Route("pizza")]
+    public class PizzaController : Controller
+    {
+        [Route("home")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("create")]
+        [HttpPost]
+        public IActionResult CreatePizza()
+        {
+            return Ok("Success");
+        }
+
+        [Route("")]
+        [HttpGet]
+        public IActionResult GetPizzaById([FromQuery] int id, [FromQuery] string name)
+        {
+            return Ok($"{id} - {name}");
+        }
+
+
+        [Route("name/{pizzaName}")]
+        [HttpGet]
+        public IActionResult GetPizzaByName(string pizzaName)
+        {
+            return Ok(pizzaName);
+        }
+    }
+}
