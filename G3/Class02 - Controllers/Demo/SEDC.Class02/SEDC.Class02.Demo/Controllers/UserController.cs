a8e560dadb16a64c2c38a068c160711858ef08c5
Martin <panovski.martin93@gmail.com>
2022-06-17 21:17:41 +0200

    Class 2
    Class 2 demo code added

---
+++ G3/Class02 - Controllers/Demo/SEDC.Class02/SEDC.Class02.Demo/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SEDC.Class02.Demo.Controllers
+{
+    [Route("корисници")]
+    public class UserController : Controller
+    {
+        [Route("сите")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("CallMe")]
+        public IActionResult Contact()
+        {
+            return new EmptyResult();
+        }
+    }
+}
