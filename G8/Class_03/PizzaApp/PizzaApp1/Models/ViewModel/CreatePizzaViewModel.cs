8c993785653d17701e89e7b3ef9bdf10966636d4
Jovan Gjorgjiev <joki.joksi@hotmail.com>
2022-06-18 12:38:55 +0200

    G8_Class_03

---
+++ G8/Class_03/PizzaApp/PizzaApp1/Models/ViewModel/CreatePizzaViewModel.cs
@@ -0,0 +1,9 @@
+﻿using PizzaApp1.Models.Enums;
+
+namespace PizzaApp1.Models.ViewModel
+{
+    public class CreatePizzaViewModel
+    {
+        public IEnumerable<PizzaSize> Sizes { get; set; } = new List<PizzaSize>();
+    }
+}
