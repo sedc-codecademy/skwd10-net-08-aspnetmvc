f99f0ec8fddc6ddc280a4d1ee9860779baba4090
rpanchevski <risto.panchevski@gmail.com>
2022-06-20 21:04:54 +0200

    Class03

---
+++ G1/Class03/ModelsDemoWebApp/ModelsDemoWebApp/Controllers/AuthorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelsDemoWebApp.Storage;
+
+namespace ModelsDemoWebApp.Controllers
+{
+    public class AuthorController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Example sending just one author to the view
+            //var author = LibraryDb.Authors.First();
+            //return View(author);
+
+            return View(LibraryDb.Authors);
+        }
+    }
+}
---
+++ G1/Class03/ModelsDemoWebApp/ModelsDemoWebApp/Controllers/BookController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelsDemoWebApp.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Book = Storage.LibraryDb.Books.First();
+            return View();
+        }
+
+        public IActionResult ViewDataAction()
+        {
+            ViewData.Add("Book", Storage.LibraryDb.Books[2]);
+            return View();
+        }
+
+        public IActionResult GetBookByName(string id = "")
+        {
+            var book = Storage.LibraryDb.Books.FirstOrDefault(x => x.Name.ToLower().StartsWith(id.ToLower()));
+
+            if(book == null)
+            {
+                return RedirectToAction("Details", "Library");
+            }
+
+            return View(book);
+        }
+    }
+}
