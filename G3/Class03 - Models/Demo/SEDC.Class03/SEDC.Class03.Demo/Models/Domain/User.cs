16ef99886be134188d3b813e52310dafa04ad2f0
Martin <panovski.martin93@gmail.com>
2022-06-20 21:15:34 +0200

    Class 03
    Class 03 demo code added

---
+++ G3/Class03 - Models/Demo/SEDC.Class03/SEDC.Class03.Demo/Models/Domain/User.cs
@@ -0,0 +1,12 @@
+﻿namespace SEDC.Class03.Demo.Models.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ G3/Class03 - Models/Demo/SEDC.Class03/SEDC.Class03.Demo/Models/ViewModels/OrderDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SEDC.Class03.Demo.Models.ViewModels
+{
+    public class OrderDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string UserFullName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string PizzaName { get; set; }
+        public double Price { get; set; }
+    }
+}
