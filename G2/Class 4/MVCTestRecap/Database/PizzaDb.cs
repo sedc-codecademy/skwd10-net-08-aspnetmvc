cc4dc6f867808ee3883e0ae3d888df3159cf78ff
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-06-21 19:30:10 +0200

    added code for partial views

--- G2/Class 4/MVCTestRecap/Database/PizzaDb.cs
+++ G2/Class 4/MVCTestRecap/Database/PizzaDb.cs
@@ -12,5 +12,19 @@ namespace Database
             new Pizza() { Id = 3, Name = "Vesuvio", Price = 7.99m, IsOnPromotion = true },
             new Pizza() { Id = 4, Name = "Oliva", Price = 15m, IsOnPromotion = true }
         };
+
+        public static List<Order> ORDERS = new List<Order>()
+        {
+            new Order() { Id = 1, CreatedAt = new DateTime(2022, 02, 02), Pizzas = new List<Pizza>()
+                {
+                PIZZAS.First(), PIZZAS.Last()
+                }
+            },
+            new Order() { Id = 2, CreatedAt = new DateTime(2019, 05, 02), Pizzas = new List<Pizza>()
+                {
+                PIZZAS.First(), PIZZAS.ElementAt(1)
+                }
+            }
+        };
     }
 }
