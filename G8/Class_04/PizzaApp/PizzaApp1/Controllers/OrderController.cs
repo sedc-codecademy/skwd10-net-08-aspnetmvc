f7ad98c6eaf76c55e549d6356639ef664f0adf69
Jovan Gjorgjiev <joki.joksi@hotmail.com>
2022-06-21 20:54:58 +0200

    G8_Class_04

---
+++ G8/Class_04/PizzaApp/PizzaApp1/Controllers/OrderController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaApp1.Models.ViewModel;
+
+namespace PizzaApp1.Controllers
+{
+    // order
+    public class OrderController : Controller
+    {
+        // default view resolving
+        // views/[controller]/[action].cshtml
+        // views/shared/[action].cshtml
+
+        // ~/views/.../ .cshtml
+
+        //public IActionResult Index()
+        //{
+        //    return View("~/Views/Home/Index.cshtml");
+        //}
+
+        public IActionResult Index()
+        {
+            var orders = PizzaAppDb.Orders.ToList();
+            return View(orders);
+        }
+        //odder/details/[1,2,3...]
+        public IActionResult Details(int id)
+        {
+            var order = PizzaAppDb.Orders.FirstOrDefault(x => x.Id == id);
+            if(order == null)
+            {
+                return new EmptyResult();
+            }
+            return View(order);
+        }
+
+        public IActionResult Create()
+        {
+            var pizzas = PizzaAppDb.Pizzas.ToList();
+            ViewBag.Pizzas = pizzas;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(OrderCreateViewModel model)
+        {
+            var pizzas = PizzaAppDb.Pizzas.ToList();
+            ViewBag.Pizzas = pizzas;
+            return View();
+        }
+    }
+}
---
+++ G8/Class_04/PizzaApp/PizzaApp1/Program.cs
@@ -0,0 +1,45 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddMvc().AddNewtonsoftJson();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+
+// Convetional dedicated route
+//blog/
+//blog/article
+//blog/asdasdasdasd
+//blog/how-to-code
+app.MapControllerRoute(
+        name: "blog",
+        pattern: "blog/{*article}/",
+        defaults: new { controller = "Blog", action = "BlogArticle" }
+    );
+// www.nasa-aplikacija/home/privacy
+//home/index/1asdads
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Pizza}/{action=Index}/{id:int?}");
+
+//app.MapControllerRoute(
+//    name: "newpattern",
+//    pattern: "{controller=Home}/{action=Index}/{article}",
+//    defaults: new { controller = "Blog", action = "BlogArticle" });
+
+
+app.Run();
