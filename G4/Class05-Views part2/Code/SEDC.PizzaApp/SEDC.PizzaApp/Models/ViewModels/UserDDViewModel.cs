47765ca88c81a44b210f93b6a69bcaa20fd0a40e
tstojanovska <stojanovskatanja03@gmail.com>
2022-06-25 12:44:49 +0200

    Class 05

---
+++ G4/Class05-Views part2/Code/SEDC.PizzaApp/SEDC.PizzaApp/Models/ViewModels/UserDDViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SEDC.PizzaApp.Models.ViewModels
+{
+    public class UserDDViewModel
+    {
+        public int Id { get; set; }
+        public string UserFullname { get; set; }
+    }
+}
