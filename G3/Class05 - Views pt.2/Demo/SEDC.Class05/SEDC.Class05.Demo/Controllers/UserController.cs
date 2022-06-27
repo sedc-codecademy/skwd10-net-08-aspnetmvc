788548a0cafd29ab65d0e2fc971855389c0f7a8f
Martin <panovski.martin93@gmail.com>
2022-06-27 17:27:49 +0200

    Class 5
    Class 05 demo code added

--- G3/Class05 - Views pt.2/Demo/SEDC.Class05/SEDC.Class05.Demo/Controllers/UserController.cs
+++ G3/Class05 - Views pt.2/Demo/SEDC.Class05/SEDC.Class05.Demo/Controllers/UserController.cs
@@ -1,66 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using SEDC.Class05.Demo.Models;
-
-namespace SEDC.Class05.Demo.Controllers
+﻿namespace SEDC.Class05.Demo.Controllers
 {
-    public class UserController : Controller
+    public class UserController
     {
-        [Route("user")]
-        public IActionResult Index(int userId)
-        {
-            var user = StaticDb.Users.SingleOrDefault(x => x.Id == 1);
-            User u = new User();
-            return View(user);
-        }
-
-        [HttpGet("all")]
-        public IActionResult Users()
-        {
-            var users = StaticDb.Users;
-            return View(users);
-        }
-
-        [HttpGet]
-        public IActionResult Create()
-        {
-            User user = new User();
-            return View(user);
-        }
-
-        [HttpPost]
-        public IActionResult Create(User user)
-        {
-            if (ModelState.IsValid)
-            {
-                user.Id = StaticDb.Users.Count() + 1;
-                StaticDb.Users.Add(user);
-                return RedirectToAction("Users");
-            }
-            return View();
-        }
-
-
-        [HttpGet]
-        public IActionResult Edit(int id)
-        {
-            var user = StaticDb.Users.SingleOrDefault(x => x.Id == id);
-            return View(user);
-        }
-
-
-        [HttpPost]
-        public IActionResult Edit(User user)
-        {
-            if (ModelState.IsValid)
-            {
-                var userFromDb = StaticDb.Users.SingleOrDefault(x => x.Id == user.Id);
-                    userFromDb.FirstName = user.FirstName;
-                    userFromDb.LastName = user.LastName;
-                    userFromDb.City = user.City;
-                    userFromDb.Age = user.Age;
-                    return RedirectToAction("Users");
-            }
-            return View();
-        }
     }
 }
