7900982936a33670e518839d3351c7cecc4ecc7c
Martin <panovski.martin93@gmail.com>
2022-06-29 19:59:14 +0200

    Class 07
    Pizza app refactored initial

---
+++ G3/Class07 - RefactorPizzaApp/Demo/SEDC.Class07/SEDC.Class07.Demo/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using SEDC.Class07.Demo.Models;
+
+namespace SEDC.Class07.Demo.Services
+{
+    public class UserService : IUserService
+    {
+        public List<User> GetAllUsers()
+        {
+            return new List<User>();
+        }
+
+        public User GetUserById()
+        {
+            return new User();
+        }
+    }
+
+    public class UserNewService : IUserService
+    {
+        public List<User> GetAllUsers()
+        {
+            return new List<User>();
+        }
+
+        public User GetUserById()
+        {
+            return new User();
+        }
+    }
+}
