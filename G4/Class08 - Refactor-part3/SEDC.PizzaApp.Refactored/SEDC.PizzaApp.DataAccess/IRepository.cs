8414631caa6a16e983315687dcf943357858069e
tstojanovska <stojanovskatanja03@gmail.com>
2022-07-04 20:59:46 +0200

    Class 08

---
+++ G4/Class08 - Refactor-part3/SEDC.PizzaApp.Refactored/SEDC.PizzaApp.DataAccess/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.DataAccess
+{
+    public interface IRepository<T>
+    {
+        //CRUD methods
+        List<T> GetAll();
+        T GetById(int id);
+        int Insert(T entity);
+        void Update(T entity);
+        void DeleteById(int id);
+    }
+}
