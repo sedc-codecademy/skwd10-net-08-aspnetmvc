9408e92ec6e5f2266d08f9a5dd584be0aa3b3a3a
Martin <panovski.martin93@gmail.com>
2022-07-04 20:05:53 +0200

    Class 08
    Class 08 demo code part 1 added

---
+++ G3/Class07&Class08 - RefactorPizzaApp/RefactoredPizzaApp/SEDC.PizzaApp/SEDC.PizzaApp.Refactored/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Mappers.Extensions;
+using SEDC.PizzaApp.Services.Services.Interfaces;
+using SEDC.PizzaApp.ViewModels.OrderViewModels;
+
+namespace SEDC.PizzaApp.Refactored.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public IActionResult Index()
+        {
+            List<OrderListViewModel> orderListViewModel = _orderService.GetAllOrders();
+            return View(orderListViewModel);
+        }
+    }
+}
