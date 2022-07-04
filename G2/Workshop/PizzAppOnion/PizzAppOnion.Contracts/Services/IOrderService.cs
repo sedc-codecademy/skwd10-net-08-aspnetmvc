2fde876c97aae6c933afb26c3df8a089f97c4326
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-07-04 22:24:35 +0200

    init for class 9

---
+++ G2/Workshop/PizzAppOnion/PizzAppOnion.Contracts/Services/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿using PizzAppOnion.Contracts.ViewModels.Order;
+
+namespace PizzAppOnion.Contracts.Services
+{
+    public interface IOrderService
+    {
+        IReadOnlyList<OrderViewModel> GetAllOrders();
+    }
+}
---
+++ G2/Workshop/PizzAppOnion/PizzAppOnion/Controllers/OrderController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PizzAppOnion.Contracts.Services;
+using PizzAppOnion.Contracts.ViewModels.Order;
+
+namespace PizzAppOnion.API.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        // GET: OrderController
+        public ActionResult Index()
+        {
+            IEnumerable<OrderViewModel> orderListViewModel = _orderService.GetAllOrders();
+
+            return View(orderListViewModel);
+        }
+
+        // GET: OrderController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: OrderController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrderController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrderController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OrderController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrderController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OrderController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
