6bca7328a8c46f7b4fa9355094db730c9a247330
rpanchevski <risto.panchevski@gmail.com>
2022-07-04 21:29:43 +0200

    Refactor part 1

---
+++ G1/PizzaApp_Refactor/PizzaWebApp/DomainModels/MenuItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace DomainModels
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public Pizza Pizza { get; set; }
+        public Size Size { get; set; }
+        public decimal Price { get; set; }
+
+        public MenuItem()
+        {
+
+        }
+
+        public MenuItem(int id, Pizza pizza, Size size, decimal price)
+        {
+            Id = id;
+            Pizza = pizza;
+            Size = size;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Pizza.Name} ({Size.Name}) [{Price.ToString("C", CultureInfo.CreateSpecificCulture("mk-MK"))}]";
+        }
+    }
+}
