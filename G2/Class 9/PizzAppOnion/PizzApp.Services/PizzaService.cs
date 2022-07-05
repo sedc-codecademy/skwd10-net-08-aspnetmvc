2ca41bf148e319e677a10a48cff9efa9ffc022d2
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-07-05 18:14:58 +0200

    code for class 9

---
+++ G2/Class 9/PizzAppOnion/PizzApp.Services/PizzaService.cs
@@ -0,0 +1,72 @@
+﻿using PizzAppOnion.Contracts.Services;
+using PizzAppOnion.Contracts.ViewModels.Pizza;
+using PizzAppOnion.Domain.Entities;
+using PizzAppOnion.Domain.Repositories;
+using PizzAppOnion.Services.Mappers;
+
+namespace PizzAppOnion.Services
+{
+    public class PizzaService : IPizzaService
+    {
+        private readonly IPizzaRepository _pizzaRepository;
+
+        public PizzaService(IPizzaRepository pizzaRepository)
+        {
+            _pizzaRepository = pizzaRepository;
+        }
+
+        public void CreatePizza(PizzaViewModel pizza)
+        {
+            Pizza newPizza = new Pizza()
+            {
+                Name = pizza.Name,
+                Price = pizza.Price,
+                IsOnPromotion = pizza.IsOnPromotion
+            };
+
+            _pizzaRepository.Insert(newPizza);
+        }
+
+        public void DeletePizza(int id)
+        {
+            _pizzaRepository.Delete(id);
+        }
+
+        public IReadOnlyList<PizzaViewModel> GetAllPizzas()
+        {
+            IReadOnlyList<Pizza> pizzas = _pizzaRepository.GetAllPizzas();
+
+            return pizzas.Select(x => x.ToPizzaViewModel()).ToArray();
+        }
+
+        public PizzaViewModel GetPizza(int id)
+        {
+            Pizza pizza = GetPizzaById(id);
+
+            return pizza.ToPizzaViewModel();
+        }
+
+        public void UpdatePizza(int id, PizzaViewModel pizza)
+        {
+            Pizza existingPizza = GetPizzaById(id);
+
+            existingPizza.Price = pizza.Price;
+            existingPizza.IsOnPromotion = pizza.IsOnPromotion;
+            existingPizza.Name = pizza.Name;
+
+            _pizzaRepository.Update(existingPizza);
+        }
+
+        private Pizza GetPizzaById(int id)
+        {
+            Pizza pizza = _pizzaRepository.GetPizza(id);
+
+            if (pizza is null)
+            {
+                throw new Exception($"Pizza with id {id} not found");
+            }
+
+            return pizza;
+        }
+    }
+}
---
+++ G2/Class 9/PizzAppOnion/PizzAppOnion.Storage/Repository/PizzaRepository.cs
@@ -0,0 +1,47 @@
+﻿using PizzAppOnion.Domain.Entities;
+using PizzAppOnion.Domain.Repositories;
+using PizzAppOnion.Storage.Database;
+
+namespace PizzAppOnion.Storage.Repository
+{
+    public class PizzaRepository : IPizzaRepository
+    {
+        public void Delete(int id)
+        {
+            Pizza pizza = GetPizza(id);
+
+            if (pizza is null)
+            {
+                throw new Exception($"Pizza with id {id} not found");
+            }
+
+            PizzaDatabase.PIZZAS.Remove(pizza);
+        }
+
+        public IReadOnlyList<Pizza> GetAllPizzas()
+        {
+            return PizzaDatabase.PIZZAS;
+        }
+
+        public Pizza GetPizza(int id)
+        {
+            return PizzaDatabase.PIZZAS.SingleOrDefault(x => x.Id == id);
+        }
+
+        public IReadOnlyList<Pizza> GetPizzas(int[] pizzaIds)
+        {
+            return PizzaDatabase.PIZZAS.Where(x => pizzaIds.Contains(x.Id)).ToArray();
+        }
+
+        public void Insert(Pizza newPizza)
+        {
+            newPizza.Id = PizzaDatabase.GetNextPizzaId();
+            PizzaDatabase.PIZZAS.Add(newPizza);
+        }
+
+        public void Update(Pizza existingPizza)
+        {
+            //noop
+        }
+    }
+}
