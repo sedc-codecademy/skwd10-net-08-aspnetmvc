f6a4e38f9dd496d196fdaaef3d882a440f5b991a
Martin <panovski.martin93@gmail.com>
2022-07-09 09:33:07 +0200

    Class 9
    Class 10 demo code added

---
+++ G3/Class10/RefactoredPizzaApp/SEDC.PizzaApp/SEDC.PizzaApp.DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using SEDC.PizzaApp.DataAccess.Data;
+using SEDC.PizzaApp.DataAccess.Repositories.Interfaces;
+using SEDC.PizzaApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.DataAccess.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            return StaticDb.Users;
+        }
+
+        public User GetById(int id)
+        {
+            return StaticDb.Users.SingleOrDefault(x => x.Id == id);
+        }
+
+        public void Insert(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
